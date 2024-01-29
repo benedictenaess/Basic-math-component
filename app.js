8d416a675b9e09f1f31470b7218768da869828b0
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-01-29 19:51:07 +0100

    divide feature

--- app.js
+++ app.js
@@ -8,25 +8,26 @@ const input2 = document.querySelector('.input2');
 
 const output = document.querySelector('.display-number');
 
-
-
 const add = () => {
 	const newValue = Number(input1.value) + Number(input2.value);
 	output.textContent = newValue
 };
-
 addButton.addEventListener('click', add);
 
 const subtract = () => {
 	const newValue = Number(input1.value) - Number(input2.value);
 	output.textContent = newValue
 }
-
 subtractButton.addEventListener('click', subtract);
 
 const multiply = () => {
 	const newValue = Number(input1.value) * Number(input2.value);
 	output.textContent = newValue
 }
-
 multiplyButton.addEventListener('click', multiply);
+
+const divide = () => {
+	const newValue = Number(input1.value) / Number(input2.value);
+	output.textContent = newValue
+}
+divideButton.addEventListener('click', divide);
